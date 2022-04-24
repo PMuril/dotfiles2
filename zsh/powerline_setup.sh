560acc1856154fb15fef5a9c81f3167897c543d1
PMuril <paolo.muril@gmail.com>
2022-04-24 16:03:57 +0200

    Updated powerline sourcing script

---
+++ zsh/powerline_setup.sh
@@ -0,0 +1,5 @@
+#! /bin/zsh
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+/usr/local/bin/powerline-daemon -q
+source /usr/local/lib/python3.9/site-packages/powerline/bindings/zsh/powerline.zsh
