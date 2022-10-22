0cba1f877e184d05762ff3057ebf7747afdca7ce
PMuril <paolo.muril@gmail.com>
2022-10-22 16:20:14 +0200

    powerline setup

--- zsh/powerline_setup.sh
+++ zsh/powerline_setup.sh
@@ -2,4 +2,8 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 /usr/local/bin/powerline-daemon -q
-source /usr/local/lib/python3.9/site-packages/powerline/bindings/zsh/powerline.zsh
+# try also
+# /System/Volumes/Data/usr/local/bin/powerline-daemon -q
+/usr/local/lib/python3.9/site-packages/powerline/bindings/zsh/powerline.zsh
+# try also
+# /System/Volumes/Data/usr/local/lib/python3.9/site-packages/powerline/bindings/zsh/powerline.zsh
