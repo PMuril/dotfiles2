ea8d25681a1a62a9d2ecf4ba690078df6c4f8071
PMuril <paolo.muril@gmail.com>
2022-11-06 14:18:40 +0100

    reorganized plugins sessions

--- nvim/lua/muril/plugins.lua
+++ nvim/lua/muril/plugins.lua
@@ -7,31 +7,28 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
+local status_ok, _ = pcall(require, "packer")
 if not status_ok then
     return
 end
 
 return require('packer').startup(function(use)
     use "wbthomason/packer.nvim" -- Have packer manage itself
-    
-    -- Lua utility functions
-    use 'nvim-lua/plenary.nvim'
 
     -- text editing
-    -- use "tpope/vim-commentary"
     use 'https://github.com/tpope/vim-repeat'
     use 'michaeljsmith/vim-indent-object' --treat indentation levels as vim text objects
     use 'https://github.com/vim-scripts/ReplaceWithRegister'
     --
     use 'mbbill/undotree'
-    use 'https://github.com/glts/vim-magnum'    --bigint library for VIM - dependency of vim-radical
-    use 'https://github.com/glts/vim-radical'     --for istantaneously converting between numerical representations
-     use 'wellle/targets.vim'                    --allows to easily target arguments inside functions
+    use {'glts/vim-radical',                --for istantaneously converting between numerical representations
+        requires = {'glts/vim-magnum'}      --bigint library for VIM - dependency of vim-radical
+    }
+    use 'wellle/targets.vim'                --allows to easily target arguments inside functions
     --
     -- text navigation
-     -- use 'unblevable/quick-scope'         --Highlights closest match when finding characters
-     use 'bkad/CamelCaseMotion'             --Enables to specify text objects inside CamelCasedStrings and underlined_strings_
+     -- use 'unblevable/quick-scope'        --Highlights closest match when finding characters
+    use 'bkad/CamelCaseMotion'             --Enables to specify text objects inside CamelCasedStrings and underlined_strings_
 
     -- airline customization
     -- use 'vim-airline/vim-airline'
@@ -61,10 +58,11 @@ return require('packer').startup(function(use)
     -- use "JoosepAlviste/nvim-ts-context-commentstring" --contex-aware comments (for files embedding multiple languages)
     use "numtostr/comment.nvim"                     -- language-aware comments
     use "nvim-treesitter/nvim-treesitter-textobjects"
-    
 
     -- fuzzy-finding
-    use 'nvim-lua/telescope.nvim'
+    use { 'nvim-lua/telescope.nvim',
+        requires = {'nvim-lua/plenary.nvim'}    -- common Lua functions collection
+    }
 
     -- lsp
     use "neovim/nvim-lspconfig"
@@ -74,7 +72,10 @@ return require('packer').startup(function(use)
     use 'lervag/vimtex'
     -- markdown
     -- use 'dhruvasagar/vim-table-mode'
- 
+    --
+    -- wikis
+    -- use 'lervag/wiki.vim'
+
     -- color schemes
     use 'navarasu/onedark.nvim'
 
